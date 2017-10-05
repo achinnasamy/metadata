51a0317f7e9a3791fade84497ffb5000460e6fb5
chinnasamy <chinnasamyad@gmail.com>
2017-10-05 09:55:04 +0530

    lxml removed

--- metadata/metadata_validator.py
+++ metadata/metadata_validator.py
@@ -1,4 +1,4 @@
-from lxml import etree
+#from lxml import etree
 
 
 class MetaDataValidator:
@@ -12,11 +12,11 @@ class MetaDataValidator:
     # Validate XML using XSD
     def validateXMLUsingXSD(self, xsd_file, xml_file):
 
-        xmlschema_doc = etree.parse(xsd_file)
-        xmlschema = etree.XMLSchema(xmlschema_doc)
-
-        xml_doc = etree.parse(xml_file)
-        result = xmlschema.validate(xml_doc)
+        # xmlschema_doc = etree.parse(xsd_file)
+        # xmlschema = etree.XMLSchema(xmlschema_doc)
+        #
+        # xml_doc = etree.parse(xml_file)
+        # result = xmlschema.validate(xml_doc)
 
         #return result
 
